b5895ec561d5c68aa12d5630f755bdb85c1de984
Rarysson Guilherme <r_guilherme12@hotmail.com>
2024-02-28 17:04:41 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Rule of Thumb
+
+An app to track users' sentiments on trending and controversial people from different fields, including politics, business, media entertainment, etc. Made during the [Zemoga Front End](https://github.com/zemoga/ui-test) challenge.
+
+## Tech
+
+-   Vue
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import AppNav from "./components/AppNav.vue";
+import AppHeader from "./components/AppHeader.vue";
+import AppBannerTop from "./components/AppBannerTop.vue";
+import AppBannerBottom from "./components/AppBannerBottom.vue";
+import AppFooter from "./components/AppFooter.vue";
+</script>
+
+<template>
+  <AppNav />
+  <AppHeader />
+  <div class="max-centered">
+    <AppBannerTop />
+
+    <main role="main">
+      <!-- Start: Implementation -->
+      👉 Your code goes here 👈
+      <!-- End: Implementation -->
+    </main>
+
+    <AppBannerBottom />
+
+    <hr role="separator" />
+
+    <AppFooter />
+  </div>
+</template>
